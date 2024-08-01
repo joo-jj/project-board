5de4698e71c7773c0b82c9d0812649db56b78740
yujinjoo <yujinjoo98@gmail.com>
2024-08-01 15:25:41 +0900

    #1 - data rest 설정

--- project-board/src/main/java/com/yjj/projectboard/repository/ArticleRepository.java
+++ project-board/src/main/java/com/yjj/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.yjj.projectboard.repository;
 
 import com.yjj.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }

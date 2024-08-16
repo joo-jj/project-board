5fa27da028b4ce4ea02f87f6986fce032d77723f
yujinjoo <yujinjoo98@gmail.com>
2024-08-16 16:50:23 +0900

    #24 - 추가 수정 반영

--- project-board/src/test/java/com/yjj/projectboard/repository/JpaRepositoryTest.java
+++ project-board/src/test/java/com/yjj/projectboard/repository/JpaRepositoryTest.java
@@ -53,7 +53,7 @@ class JpaRepositoryTest {
     void givenTestData_whenInserting_thenWorksFine() {
         // Given
         long previousCount = articleRepository.count();
-        UserAccount userAccount = userAccountRepository.save(UserAccount.of("yjj","pw",null,null,null));
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("newYjj","pw",null,null,null));
         Article article = Article.of(userAccount, "new article", "new content", "#spring");
 
         // When

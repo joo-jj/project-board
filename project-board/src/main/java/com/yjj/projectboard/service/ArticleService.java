e65cfd4131e2c72079785be5a1d6786bd5cf94b9
yujinjoo <yujinjoo98@gmail.com>
2024-08-19 17:10:14 +0900

    #27 - 게시글 조회 오류 수정

--- project-board/src/main/java/com/yjj/projectboard/service/ArticleService.java
+++ project-board/src/main/java/com/yjj/projectboard/service/ArticleService.java
@@ -68,6 +68,10 @@ public class ArticleService {
         articleRepository.deleteById(articleId);
     }
 
+    public long getArticleCount(){
+        return articleRepository.count();
+    }
+
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticlesViaHashtag(String hashtag, Pageable pageable) {
         if(hashtag == null || hashtag.isBlank()) {

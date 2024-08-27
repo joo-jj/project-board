dbc2d0ed79ff494103ebba64350bd976cd6a825e
yujinjoo <yujinjoo98@gmail.com>
2024-08-27 15:49:03 +0900

    #41 - 대댓글 비즈니스 로직 구현 + 댓글 컨트롤러 테스트 업데이트: 대댓글 케이스 추가

--- project-board/src/main/java/com/yjj/projectboard/dto/request/ArticleCommentRequest.java
+++ project-board/src/main/java/com/yjj/projectboard/dto/request/ArticleCommentRequest.java
@@ -9,14 +9,21 @@ import java.time.LocalDateTime;
 /**
  * DTO for {@link com.yjj.projectboard.domain.ArticleComment}
  */
-public record ArticleCommentRequest(Long articleId, String content) {
+public record ArticleCommentRequest
+        (Long articleId,
+         Long parentCommentId,
+         String content) {
     public static ArticleCommentRequest of(Long articleId, String content) {
-        return new ArticleCommentRequest(articleId, content);
+        return ArticleCommentRequest.of(articleId, null, content);
+    }
+
+    public static ArticleCommentRequest of(Long articleId,Long parentCommentId, String content) {
+        return new ArticleCommentRequest(articleId, parentCommentId, content);
     }
 
     public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
         return ArticleCommentDto.of(
-                articleId, userAccountDto, content
+                articleId, userAccountDto, parentCommentId, content
         );
     }
 }
